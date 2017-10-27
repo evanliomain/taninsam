23ee837fb710fab34831ac9b65cecc9c410e2d19
Evan Liomain <evan.liomain@gmail.com>
2017-10-27 19:17:14 +0200

    fix($compile): Fix rollup bundle
    Add a main file to export all functions and use name from package.json

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,23 +1,23 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import camelCase from 'lodash.camelcase';
 
-const pkg = require('./package.json')
+const pkg = require('./package.json');
 
-const libraryName = 'taninsam'
+const libraryName = pkg.name;
 
 export default {
   input: `compiled/${libraryName}.js`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.module, format: 'es' }
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
-    include: 'compiled/**',
+    include: 'compiled/**'
   },
   plugins: [
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
@@ -28,6 +28,6 @@ export default {
     resolve(),
 
     // Resolve source maps to the original source
-    sourceMaps(),
-  ],
-}
+    sourceMaps()
+  ]
+};
