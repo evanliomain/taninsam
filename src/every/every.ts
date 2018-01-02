373eff4cbf3a12672cf51fccea29aae5153ffe78
Evan Liomain <evan.liomain@gmail.com>
2018-01-02 17:40:23 +0100

    feat(every): Add the every function on array

---
+++ src/every/every.ts
@@ -0,0 +1,23 @@
+/**
+ * @module array=>boolean
+ */
+/**
+ * Determines whether all the members of an array satisfy the specified test.
+ * @param iteratee A function that accepts up to three arguments.
+ * @return the function to apply on the array to determine if its satisfy the specified test
+ * @example
+ * ```
+ * every<number>(x => x < 6)([1, 2, 3, 4, 5]) // true
+ * ```
+ * @example Using the chain
+ * ```
+ * chain([1, 2, 3, 4, 5])
+ *   .chain(every<number>(x => x < 6))
+ *   .value() // true
+ * ```
+ */
+export function every<T>(
+  iteratee: (value: T, index: number, array: ReadonlyArray<T>) => boolean
+): (array: ReadonlyArray<T>) => boolean {
+  return (array: ReadonlyArray<T>) => array.every(iteratee);
+}
--- src/taninsam.ts
+++ src/taninsam.ts
@@ -1,6 +1,7 @@
 export * from './chain';
 export * from './chainFn';
 export * from './entries';
+export * from './every';
 export * from './filter';
 export * from './keys';
 export * from './map';
