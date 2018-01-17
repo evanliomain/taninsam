bfeace4e9798227101ec9afc72017fde3a75e853
Evan Liomain <evan.liomain@gmail.com>
2018-01-17 18:04:20 +0100

    feat(isUndefined): Add type checking isUndefined method

---
+++ src/isUndefined/isUndefined.ts
@@ -0,0 +1,19 @@
+/**
+ * @module any=>boolean
+ */
+/**
+ * Determines whether the passed value is undefined
+ * @param value The value to test.
+ * @return true if the value is undefined, false otherwise
+ * @example
+ * ```
+ * isUndefined(undefined) // true
+ * isUndefined(null) // false
+ * isUndefined([]) // false
+ * isUndefined('') // false
+ * isUndefined(0) // false
+ * ```
+ */
+export function isUndefined(value): value is undefined {
+  return 'undefined' === typeof value && undefined === value;
+}
