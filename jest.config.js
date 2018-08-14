01dccb1277c56e733d1ea40060a2045fc2199b4d
Evan Liomain <evan.liomain@gmail.com>
2018-08-14 14:21:16 +0200

    ci(jest config): Fix the jest configuration
    Fix the error "Jest SecurityError: localStorage is not available for opaque origins"

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '.(ts|tsx)': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  },
+  collectCoverage: true
+};
