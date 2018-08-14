410ec6eedd1379ad696f5a41efdb597a7e59127f
Grégory Barale <barale.g@sfeir.com>
2018-08-14 11:20:10 +0200

    docs(tail): Update documentation for the tail function file
    Change the description of the description of the tail function. Change the description of the return
    of the tail function.

--- src/tail/tail.ts
+++ src/tail/tail.ts
@@ -2,8 +2,8 @@
  * @module array=>array
  */
 /**
- * Extract the tail of an array and return an empty array if the original one is empty
- * @return the function to apply on the TBD to do something
+ * Return the array without the first element. Return an empty array if the input is an empty array.
+ * @return the function to apply on the array to do return it without the first element
  * @example
  * ```
  * tail<number>()([1, 2, 3, 4, 5]) // [2, 3, 4, 5]
