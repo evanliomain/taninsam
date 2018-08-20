e2eae619fd0c00a144fcbf894c82a1f51c2edae2
Grégory Barale <barale.g@sfeir.com>
2018-08-20 23:39:48 +0200

    test(castTo): Replace test in castTo spec
    Replace test for more meaningfull one in spec for castTo function

--- src/castTo/castTo.spec.ts
+++ src/castTo/castTo.spec.ts
@@ -9,18 +9,27 @@ describe('castTo function', () => {
     expect(input).toMatchSnapshot();
   });
 
-  test('{a: 1, b: 2, c:3} |> castTo === {a: 2, b: 2, c: 3}', () => {
-    expect(castTo({ a: x => x + x, b: x => x })(input)).toEqual({
-      a: 2,
-      b: 2,
-      c: 3
-    });
-  });
+  test(
+    "{ a: '1', b: 2.1, c: 'A' }" +
+      ' |> castTo({a: x => parseInt(x.a, 10),b: x => Math.floor(x.B),c: x => parseInt(x.c, 16)})' +
+      ' === { a: 1, b: 2, c: 10 }',
+    () => {
+      expect(
+        castTo({
+          a: x => parseInt(x.a, 10),
+          b: x => Math.floor(x.B),
+          c: x => parseInt(x.c, 16)
+        })(input)
+      ).toMatchSnapshot();
+    }
+  );
 
-  test('[{a: 1, b: 2, c:3}, {a: 4, b: 5, c:6}] |> castTo === [{a: 2, b: 2, c: 3}, {a: 8, b: 5, c: 6}]', () => {
-    expect(castTo({ a: x => x + x, b: x => x })(array)).toEqual([
-      { a: 2, b: 2, c: 3 },
-      { a: 8, b: 5, c: 6 }
-    ]);
-  });
+  test(
+    '[{a: 1, b: 2, c:3}, {a: 4, b: 5, c:6}]' +
+      ' |> castTo({ a: x => x + x, b: x => x })' +
+      ' === [{a: 2, b: 2, c: 3}, {a: 8, b: 5, c: 6}]',
+    () => {
+      expect(castTo({ a: x => x + x, b: x => x })(array)).toMatchSnapshot();
+    }
+  );
 });
