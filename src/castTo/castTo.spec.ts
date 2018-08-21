9c613de6eafe454d612f14a82e79887141e60c10
Grégory Barale <barale.g@sfeir.com>
2018-08-21 22:27:49 +0200

    test(castTo): Update test
    Update test and snapshot to fix NaN values in snapshot

--- src/castTo/castTo.spec.ts
+++ src/castTo/castTo.spec.ts
@@ -1,7 +1,7 @@
 import { castTo } from './castTo';
 
 describe('castTo function', () => {
-  const input = { a: 1, b: 2, c: 3 };
+  const input = { a: 1, b: 2, c: 'A' };
   const array = [{ a: 1, b: 2, c: 3 }, { a: 4, b: 5, c: 6 }];
 
   test('is a pure function', () => {
@@ -11,14 +11,14 @@ describe('castTo function', () => {
 
   test(
     "{ a: '1', b: 2.1, c: 'A' }" +
-      ' |> castTo({a: x => parseInt(x.a, 10),b: x => Math.floor(x.B),c: x => parseInt(x.c, 16)})' +
+      ' |> castTo({a: x => parseInt(x, 10),b: x => Math.floor(x),c: x => parseInt(x, 16)})' +
       ' === { a: 1, b: 2, c: 10 }',
     () => {
       expect(
         castTo({
-          a: x => parseInt(x.a, 10),
-          b: x => Math.floor(x.B),
-          c: x => parseInt(x.c, 16)
+          a: x => parseInt(x, 10),
+          b: x => Math.floor(x),
+          c: x => parseInt(x, 16)
         })(input)
       ).toMatchSnapshot();
     }
