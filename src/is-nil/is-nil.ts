60bb5958b063c4e639159bb99407dcd44bb810d4
Evan Liomain <evan.liomain@gmail.com>
2019-10-11 07:56:10 +0200

    feat(isnil): add isNil function
    check if the input is undefined or null

---
+++ src/is-nil/is-nil.ts
@@ -0,0 +1,30 @@
+import { isNull } from '../is-null';
+import { isUndefined } from '../is-undefined';
+
+/**
+ * @module any=>boolean
+ */
+/**
+ * Determines whether the passed value is undefined or null.
+ * @param value The value to test.
+ * @return true if the value is undefined or null, false otherwise
+ * @example
+ * ```
+ * isNil(undefined) // true
+ * isNil(null) // true
+ * isNil([]) // false
+ * isNil({}) // false
+ * isNil('') // false
+ * isNil(0) // false
+ * ```
+ */
+export function isNil(value): value is undefined | null {
+  if (isUndefined(value)) {
+    return true;
+  }
+  if (isNull(value)) {
+    return true;
+  }
+
+  return false;
+}
