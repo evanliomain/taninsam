7173f69b705df92fcbb6c1ce86bb6bba24fa0396
Evan Liomain <evan.liomain@gmail.com>
2019-10-13 18:12:41 +0200

    feat(loopwhile): add loopWhile function
    loopWhile perform a loop over an input until a predicate is false

---
+++ src/loop-while/loop-while.ts
@@ -0,0 +1,36 @@
+/**
+ * @module any=>any
+ */
+/**
+ * This method loop on an input:
+ * - at first, output = input
+ * - while predicat(output) is true, output = iteratee(output)
+ * - finally, returns output
+ * @param predicate The function apply on each loop to decide to :
+ * continue (return true), or to stop (return false)
+ * @param iteratee The iteratee invoked on each loop.
+ * @return the function to apply on the input
+ * @example
+ * ```
+ * loopWhile<number>(x => x < 10, x => x^2)(2) // 16
+ * ```
+ * @example Using the chain
+ * ```
+ * chain(2)
+ *   .chain(loopWhile<number>(x => x < 10, x => x^2))
+ *   .value() // 16
+ * ```
+ */
+export function loopWhile<T>(
+  predicate: (element: T) => boolean,
+  iteratee: (element: T) => T
+): (input: T) => T {
+  return (input: T) => {
+    let output = input;
+    while (predicate(output)) {
+      output = iteratee(output);
+    }
+
+    return output;
+  };
+}
