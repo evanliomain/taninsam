5d88d52c325c1b86527bcd843f009738b110627e
Evan Liomain <evan.liomain@gmail.com>
2019-10-13 17:42:06 +0200

    feat(loopfor): add loopFor function
    loopFor perform a for loop over an input

---
+++ src/loop-for/loop-for.spec.ts
@@ -0,0 +1,23 @@
+import { map } from '../map';
+import { loopFor } from './loop-for';
+
+describe('loopFor function', () => {
+  const input = [1, 2, 3, 4, 5];
+
+  test('is a pure function', () => {
+    loopFor<ReadonlyArray<number>>(5, array =>
+      map<number, number>(x => 1 + x)(array)
+    )(input);
+    expect(input).toMatchSnapshot();
+  });
+
+  test('increment a number', () => {
+    expect(loopFor<number>(5, x => 1 + x)(1)).toBe(6);
+  });
+
+  test('sum even square', () => {
+    expect(
+      loopFor<number>({ start: 2, stop: 10, step: 2 }, (x, i) => x + i ** 2)(0)
+    ).toBe(120);
+  });
+});
---
+++ src/loop-for/loop-for.ts
@@ -0,0 +1,65 @@
+import { isNumber } from '../is-number';
+
+/**
+ * @module any=>any
+ */
+interface Iteration {
+  start?: number;
+  stop: number;
+  step?: number;
+}
+interface IterationParameters {
+  start: number;
+  stop: number;
+  step: number;
+}
+/**
+ * This method loop on an input:
+ * - at first, output = input
+ * - on each call, it output = iteratee(output)
+ * - the loop is parameterize by iteration as a number of loop to apply,
+ * or a start, stop, step object
+ * - finally, returns output
+ * @param iteration The number of iteration, or a start, stop, step object.
+ * @param iteratee The iteratee invoked per element.
+ * @return the function to apply on the input
+ * @example
+ * ```
+ * loopFor<number>(5, x => 1 + x)(1) // 6
+ * ```
+ * @example with iteration object
+ * ```
+ * loopFor<number>({ start: 2, stop: 10, step: 2 }, (x, i) => x + i^2)(0)
+ * // 2² + 4² + 6² + 8² = 120
+ * ```
+ * @example Using the chain
+ * ```
+ * chain(1)
+ *   .chain(loopFor<number>(5, x => 1 + x))
+ *   .value() // 6
+ * ```
+ */
+export function loopFor<T>(
+  iteration: number | Iteration,
+  iteratee: (x: T, index?: number) => T
+): (input: T) => T {
+  const { start, stop, step } = forParameters(iteration);
+
+  return (input: T) => {
+    let accumulator = input;
+    for (let index = start; index < stop; index += step) {
+      accumulator = iteratee(accumulator, index);
+    }
+
+    return accumulator;
+  };
+}
+
+function forParameters(iteration: number | Iteration): IterationParameters {
+  const defaultParameters = { start: 0, step: 1 };
+  if (isNumber(iteration)) {
+    return { ...defaultParameters, stop: iteration };
+  }
+
+  return { ...defaultParameters, ...iteration };
+}
