a0f94fe4cf78d6909ea80f0934ff5d7e8d5a1f53
Evan Liomain <evan.liomain@gmail.com>
2019-10-27 16:44:22 +0100

    ci(test): generate coverage as html in the docs directory, to be available online

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
   testRegex: '(/__tests__/.*|\\.spec)\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js'],
   coveragePathIgnorePatterns: ['/node_modules/', '/test/', '/stryker-tmp'],
+  coverageReporters: ['json', 'lcov', 'text', 'text-summary', 'clover', 'html'],
+  coverageDirectory: 'dist/docs/coverage',
   coverageThreshold: {
     global: {
       branches: 100,
