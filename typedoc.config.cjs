26236de13f6bfbb535eadbd84735c05bbfdd93bc
Evan Liomain <evan.liomain@ext.viragegroup.com>
2024-03-24 10:13:16 +0100

    chore(package): update typedoc

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -1,12 +1,12 @@
+/** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
   out: 'dist/docs',
   json: 'dist/docs/out.json',
   name: 'taninsam',
   exclude: '**/*+(index|.spec).ts',
-  mode: 'file',
-  target: 'es6',
   excludePrivate: true,
   excludeProtected: true,
   categorizeByGroup: false,
-  theme: 'node_modules/typedoc-default-themes-bugfixed/bin/default'
+  plugin: ["typedoc-material-theme"],
+  themeColor: "#fff",
 };
