e4183c2e3de3c694dc4100abf5a54562dd31e97a
Evan Liomain <evan.liomain@ext.viragegroup.com>
2024-04-07 16:55:44 +0200

    chore(documentation): Fix documentation deployment

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://evanliomain.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/taninsam/',
 
   // GitHub pages deployment config.
   organizationName: 'evanliomain', // Usually your GitHub org/user name.
